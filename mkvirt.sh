9a11fb8a94971e764fb1fd0c43c8c38341f11d7d
Isreal Varela <isrealv@gmail.com>
2024-04-12 22:16:30 -0400

    Update add packages to via kickstart

--- mkvirt.sh
+++ mkvirt.sh
@@ -4,11 +4,11 @@ virt-install --noreboot \
 	--name rocky9-template \
 	--memory 4096 \
 	--vcpus=4 \
-        --os-variant rhel9.0 \
-       	--accelerate \
-	--import \
-	--network default,model=virtio \
-	--disk path=/srv/vmdisks/rocky9-template.img,size=40 \
+    --os-variant rhel9.0 \
+	--os-type linux \
+    --accelerate \
+	--network bridge=br0,model=virtio \
+	--disk path=/srv/vmdisks/rocky9-template.qcow2,size=40 \
 	--initrd-inject=rocky9.cfg \
 	--extra-args='inst.ks=file:rocky9.cfg console=tty0 console=ttyS0,9600' \
-	--location /srv/iso/Rocky-9.3-x86_64-minimal.iso
+	--location /srv/iso/Rocky-9.3-x86_64-dvd.iso
---
+++ notes.md
@@ -0,0 +1,2 @@
+https://sysguides.com/create-and-manage-storage-pools-and-volumes-in-kvm
+https://computingforgeeks.com/install-use-kvm-virtualization-on-rocky/
