ac56691277e93dd9f508975051bb9f2002713427
Isreal Varela <isrealv@gmail.com>
2024-04-17 14:00:18 -0400

    Fix typo in README, update naming for vm to include date

--- mkvirt.sh
+++ mkvirt.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 virt-install --noreboot \
-  --name rocky9-template \
+  --name rocky9-${date} \
   --memory 4096 \
   --vcpus=4 \
   --os-variant rhel9.0 \
   --os-type linux \
   --accelerate \
   --network bridge=br0,model=virtio \
-  --disk path=/srv/vmdisks/rocky9-template.qcow2,size=40 \
+  --disk path=/srv/vmdisks/rocky9-${date}.qcow2,size=40 \
   --initrd-inject=rocky9.cfg \
   --extra-args='inst.ks=file:rocky9.cfg console=tty0 console=ttyS0,9600' \
   --location /srv/iso/Rocky-9.3-x86_64-dvd.iso
