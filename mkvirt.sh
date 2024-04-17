a29f0c54d80bcef54cf1c9f5b54156013ac1320c
Isreal Varela <isrealv@gmail.com>
2024-04-17 14:24:07 -0400

    Add date for vm name, accept eula

--- mkvirt.sh
+++ mkvirt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+date=$(date '+%Y-%m-%d')
+
 virt-install --noreboot \
   --name rocky9-${date} \
   --memory 4096 \
@@ -11,4 +13,6 @@ virt-install --noreboot \
   --disk path=/srv/vmdisks/rocky9-${date}.qcow2,size=40 \
   --initrd-inject=rocky9.cfg \
   --extra-args='inst.ks=file:rocky9.cfg console=tty0 console=ttyS0,9600' \
-  --location /srv/iso/Rocky-9.3-x86_64-dvd.iso
+  --location /srv/iso/Rocky-9.3-x86_64-dvd.iso \
+  --noautoconsole
+
