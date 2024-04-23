b64b3f725574bb834bea2420f3c89fd3e2ce6132
Isreal Varela <isrealv@gmail.com>
2024-04-23 16:07:20 -0400

    Add FIPS mode

--- mkvirt.sh
+++ mkvirt.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
+#
+# Script is used to build kvm image.
+# It will back up the vm template adding the current date and then delete the qcow2 image as well as remove the kvm from inventory before attempting to rebuild.
+
 
 date=$(date '+%Y-%m-%d')
 base_dir="/srv/vmdisks"
-vm_name="rocky9-template"
-vm_backup="rocky9-${date}.qcow2"
 backup="true"
 
 
 function backup() {
-  mv -f ${base_dir}/${vm_name}.qcow2 ${base_dir}/${vm_backup}
+  mv -f "${base_dir}"/"${vm_name}".qcow2 "${base_dir}"/"${vm_backup}"
 }
 
 function create_virt() {
   virt-install --noreboot \
-  --name ${vm_name} \
+  --name "${vm_name}" \
   --memory 4096 \
   --vcpus=4 \
-  --os-variant rhel9.0 \
+  --os-variant rocky9 \
   --accelerate \
   --network bridge=br0,model=virtio \
-  --disk path=/srv/vmdisks/${vm_name}.qcow2,size=40 \
+  --disk path=/srv/vmdisks/"${vm_name}".qcow2,size=40 \
   --initrd-inject=rocky9.cfg \
-  --extra-args='inst.ks=file:rocky9.cfg console=tty0 console=ttyS0,9600' \
+  --extra-args='inst.ks=file:rocky9.cfg fips=1 console=tty0 console=ttyS0,9600' \
   --location /srv/iso/Rocky-9.3-x86_64-dvd.iso \
   --noautoconsole
 }
 
+if [[ -n ${1} ]];then
+	vm_name=${1}
+else
+	vm_name="rocky9-template"
+fi
+
+vm_backup="${vm_name}-${date}.qcow2"
+
 if [[ -n ${backup} ]];then
   backup
-  virsh undefine ${vm_name}
+  virsh undefine "${vm_name}"
   create_virt
 else
-  virsh undefine ${vm_name}
+  virsh undefine "${vm_name}"
   create_virt
 fi
 
--- notes.md
+++ notes.md
@@ -1,2 +1,4 @@
 https://sysguides.com/create-and-manage-storage-pools-and-volumes-in-kvm
 https://computingforgeeks.com/install-use-kvm-virtualization-on-rocky/
+
+adding fips=1 to ks boot and bootloader
